f6a5613c08e57a17eb86186b62db41963e9ff9d7
Paulo Pitte <paulopitte@gmail.com>
2019-12-07 21:03:17 -0300

    Inicial

---
+++ EFCorePratica/Data/LivroContext.cs
@@ -0,0 +1,34 @@
+﻿using EFCorePratica.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePratica.Data
+{
+    public class LivroContext : DbContext
+
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore.pratica;Trusted_Connection=True;");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Livro>()
+                .ToTable("Livros");
+
+            modelBuilder.Entity<Livro>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Livro>()
+                .Property(x => x.Titulo)
+                .HasColumnType("varchar(50)");
+        }
+
+    }
+}
---
+++ EFCorePratica/Model/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCorePratica.Model
+{
+   public  class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public int AnoPublicacao { get; set; }
+
+    }
+}
